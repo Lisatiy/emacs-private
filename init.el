4584a23debd2f016d9e63828cf0b6b25982d4670
Lisatiy <lsfshufei@163.com>
2020-04-28 14:32:57 +0800

    'initial-init'

---
+++ init.el
@@ -0,0 +1,115 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+					   
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+;; cl - Common Lisp Extension
+(require 'cl)
+;; Add Packages
+(defvar lisatiy/packages '(
+			   ;; --- Auto-completion ---
+			   company
+			   ;; --- Better Editor ---
+			   hungry-delete
+			   swiper
+			   counsel
+			   smartparens
+			   ;; --- Major Mode ---
+			   js2-mode
+			   ;; --- Minor Mode ---
+			   nodejs-repl
+			   exec-path-from-shell
+			   ;; --- Themes ---
+			   monokai-theme
+			   ;; solarized-theme
+			   ) "Default packages")
+(setq package-selected-packages lisatiy/packages)
+(defun lisatiy/packages-installed-p ()
+  (loop for pkg in lisatiy/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+(unless (lisatiy/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg lisatiy/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(require 'hungry-delete)  
+(global-hungry-delete-mode)
+
+(require 'smartparens-config)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(add-hook 'js-mode-hook 'smartparens-mode)
+
+;; cofig js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+(require 'nodejs-repl)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+ (global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-splash-screen 1)
+(global-linum-mode t)
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+(global-company-mode t)
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(delete-selection-mode t)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(global-hl-line-mode t)
+(load-theme 'monokai t)
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+(setq org-agenda-files '("~/org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-external-variable ((t (:foreground "dark gray")))))
